9c2d2a8b871a82493187e8729e7e96f53cf885fa
Махмудов Дилшод <ahmadsho240480@gmail.com>
2026-09-06 12:43:22 +0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.go
@@ -0,0 +1,15 @@
+func twoSum(nums []int, target int) []int {
+
+	n:= make(map[int]int);
+
+	for i, f := range nums {
+		t := target - f;
+		if ind, t := n[t]; t{
+			return []int{ind, i}
+		}
+		n[f] = i;
+	}
+
+	return nil;
+    
+}
